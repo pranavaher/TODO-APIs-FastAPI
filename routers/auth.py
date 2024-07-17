e983148b74d8ce49b38ca193987376bf1bf3e934
Pranav Aher <doingdifferent29@gmail.com>
2024-07-17 10:37:16 +0530

    Create CreateUserRequest pydantic class

--- routers/auth.py
+++ routers/auth.py
@@ -1,6 +1,15 @@
 from fastapi import APIRouter
+from pydantic import BaseModel, Field
 
 router = APIRouter()
+
+class CreateUserRequest(BaseModel):
+  email = str
+  username = str
+  first_name = str
+  last_name = str
+  password = str
+  role = str
 
 @router.get("/auth/")
 async def get_user():
