f8762b7975e511d265d83b67acf37b6161457af9
Pranav Aher <doingdifferent29@gmail.com>
2024-07-17 10:30:26 +0530

    Add new database

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHAMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHAMY_DATABASE_URL = "sqlite:///./todosapp.db"
 
 engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
