6f32b14a6d1429f1f79664175efa5433b6ac8dd5
Pranav Aher <doingdifferent29@gmail.com>
2024-07-17 11:18:46 +0530

    Add hashed_password
    Add user db

--- routers/auth.py
+++ routers/auth.py
@@ -1,27 +1,43 @@
-from fastapi import APIRouter
-from pydantic import BaseModel, EmailStr
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from database import SessionLocal
+from sqlalchemy.orm import Session
 from models import Users
+from starlette import status
+from passlib.context import CryptContext
 
 router = APIRouter()
 
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 class CreateUserRequest(BaseModel):
-  email: EmailStr
+  email: str
   username: str
   first_name: str
   last_name: str
   password: str
   role: str
 
-@router.post("/auth/")
-async def create_user(create_user_request: CreateUserRequest):
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
   create_user_model = Users(
     email = create_user_request.email,
     username = create_user_request.username,
     first_name = create_user_request.first_name,
     last_name = create_user_request.last_name,
     role = create_user_request.role,
-    hashed_password = create_user_request.password,
+    hashed_password = bcrypt_context.hash(create_user_request.password),
     is_active = True
   )
 
-  return create_user_model
+  db.add(create_user_model)
+  db.commit()
