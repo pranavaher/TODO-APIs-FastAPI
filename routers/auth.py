f6df7b8d9557335b1f5a70bd371959fcbab02804
Pranav Aher <doingdifferent29@gmail.com>
2024-07-17 10:43:43 +0530

    Add create user route

--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel, Field
+from models import Users
 
 router = APIRouter()
 
@@ -11,6 +12,16 @@ class CreateUserRequest(BaseModel):
   password = str
   role = str
 
-@router.get("/auth/")
-async def get_user():
-  return { "message": "authenticated" }
+@router.post("/auth/")
+async def create_user(create_user_request: CreateUserRequest):
+  create_user_model = Users(
+    email = create_user_request.email,
+    username = create_user_request.username,
+    first_name = create_user_request.first_name,
+    last_name = create_user_request.last_name,
+    role = create_user_request.role,
+    hashed_password = create_user_request.password,
+    is_active = True
+  )
+
+  return create_user_model
