bd4728246473e60e895d9f90cf6218a20a263228
Pranav Aher <doingdifferent29@gmail.com>
2024-07-19 11:32:33 +0530

    Authenticate update todo route

--- routers/todos.py
+++ routers/todos.py
@@ -37,7 +37,7 @@ async def read_todo(user: user_dependency, db: db_dependency, todo_id: int = Pat
   if user is None:
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed")
   
-  todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+  todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get("id")).first()
   if todo_model is not None:
     return todo_model
   raise HTTPException(status_code=404, detail="Todo not found")
@@ -52,8 +52,10 @@ async def create_todo(user: user_dependency, db: db_dependency, todo_request: To
   db.commit()
 
 @router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
-  todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+async def update_todo(user: user_dependency, db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+  if user is None:
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed")
+  todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get("id")).first()
   if todo_model is None:
     raise HTTPException(status_code=404, detail="Todo not found")
   
