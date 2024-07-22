f08360130e097efc176d21dfc0c7e5a6f93be6ed
Pranav Aher <doingdifferent29@gmail.com>
2024-07-22 10:21:38 +0530

    Add create address route

--- routers/address.py
+++ routers/address.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('..')
 
 from typing import Optional
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -29,3 +29,23 @@ class Address(BaseModel):
   zip: str
   country: str
 
+@router.post("/")
+def create_address(address: Address, db: Session = Depends(get_db), user: dict = Depends(get_current_user)):
+  if user is None:
+    raise HTTPException(status_code=401, detail="Authentication Failed")
+  address_model = models.Address()
+
+  address_model.street = address.street
+  address_model.city = address.city
+  address_model.state = address.state
+  address_model.zip = address.zip
+  address_model.country = address.country
+
+  db.add(address_model)
+  db.flush()
+  
+  user_model = db.query(models.Users).filter(models.Users.id == user.get("id")).first()
+  user_model.address_id = address_model.id
+
+  db.add(user_model)
+  db.commit()
